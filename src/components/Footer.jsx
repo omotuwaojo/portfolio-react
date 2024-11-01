01704094cbbb16620b63dbeba2d826b898dcc2ee
Omotuwa Ojo <75334653+omotuwaojo@users.noreply.github.com>
2024-11-01 07:47:19 -0700

    footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
-import { FaFacebookSquare } from 'react-icons/fa';
-import { FaLinkedin } from "react-icons/fa6";
-import { FaWhatsappSquare } from "react-icons/fa";
-import { Link, NavLink } from "react-router-dom";
+import { FaFacebookSquare, FaInstagramSquare, FaLinkedin, FaTwitterSquare,FaWhatsappSquare } from 'react-icons/fa';
 
 
 export default function Footer() {
-  return (
-    <div className='flex'> 
-      <Link to=""><FaFacebookSquare /></Link>
-      <Link to=""><FaWhatsappSquare /></Link>
-      <Link to=""><FaLinkedin /></Link>
-      <Link to=""><FaLinkedin /></Link>
-    </div>
-  )
-}
+  return(
+  <footer className="bg-white py-4">
+  <div className="flex justify-center space-x-6">
+    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+      <FaFacebookSquare className="text-gray-600 hover:text-blue-600" size={34} />
+    </a>
+    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+      <FaTwitterSquare className="text-gray-600 hover:text-blue-400" size={34} />
+    </a>
+    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+      <FaLinkedin className="text-gray-600 hover:text-blue-700" size={34} />
+    </a>
+    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <FaInstagramSquare className="text-gray-600 hover:text-blue-500" size={34} />
+    </a>
+    <a href="https://whatsapp.com" target="_blank" rel="noopener noreferrer">
+      <FaWhatsappSquare className="text-gray-600 hover:text-blue-500" size={34} />
+    </a>
+  </div>
+</footer>
+);
+};
